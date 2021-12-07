8da9035433f88d0a14c6ea63f90873a0fe358987
eXtreme Training <61291067+Sofka-XT@users.noreply.github.com>
2021-12-07 09:44:24 -0500

    Add files via upload

---
+++ src/functions.test.js
@@ -0,0 +1,23 @@
+import { createEvent } from './functions'
+
+test('Validation a event title and content basic', () => {
+    //TODO: hacer las verificaciones
+});
+
+test('Validation start date', () => {
+    //TODO: hacer las verificaciones
+});
+
+test('Validation date', () => {
+   //TODO: hacer las verificaciones
+});
+
+
+test('Validation illegal arguments', () => {
+    //TODO: hacer las verificaciones
+});
+
+
+test('create an event list of at least 10 events', () => {
+    //TODO: hacer las verificaciones
+});
